ea0f15d0f6cefc8a2db881a23b1c7a60834c3c41
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2016-10-09 12:46:01 +0300

    Usage example updated with how to run it.

--- examples/src/main/scala/fi/helsinki/cs/nodes/examples/OptMainUsageExample.scala
+++ examples/src/main/scala/fi/helsinki/cs/nodes/examples/OptMainUsageExample.scala
@@ -4,8 +4,21 @@ import fi.helsinki.cs.nodes.util.OptMain
 
 /**
  * Example of OptMain usage.
- * Usage of this class:
- * fi.helsinki.cs.nodes.examples.OptMainUsageExample -t10000 --regs someregs --samples somesamples --target sometarget --rates pathy --load
+ * Usage of this class with maven:
+ mvn scala:run -DmainClass=fi.helsinki.cs.nodes.examples.OptMainUsageExample -DaddArgs="-t10000|--regs|someregs|--samples|somesamples|--target|sometarget|--rates|pathy|--load"
+ With scala, e.g.:
+ scala -cp target/classes:$HOME/.m2/repository/fi/helsinki/cs/nodes/getopt-scala/1.1.0/getopt-scala-1.1.0.jar fi.helsinki.cs.nodes.examples.OptMainUsageExample -t10000 --regs someregs --samples somesamples --target sometarget --rates pathy --load
+ With scala repl:
+   val args = "-t10000 --regs someregs --samples somesamples --target sometarget --rates pathy --load".split(" ")
+   args: Array[String] = Array(-t10000, --regs, someregs, --samples, somesamples, --target, sometarget, --rates, pathy, --load)
+   scala> fi.helsinki.cs.nodes.examples.OptMainUsageExample.main(args)
+   regs was set to someregs
+   samples was set to somesamples
+   Optional option rates was set to pathy
+   target was set to sometarget
+   tasks was set to 10000
+   load was given on the command line
+   
  *  
  * @copyright University of Helsinki
  * @author Eemil Lagerspetz
@@ -39,7 +52,16 @@ object OptMainUsageExample extends OptMain {
   /**
    * Main work function.
    */
-  def optMainUsageExample(regs: String, singlesamples: String, rates:Option[String], target: String, tasks: Int, load: Boolean) {
-    // TODO: Do your work here.
+  def optMainUsageExample(regs: String, samples: String, rates:Option[String], target: String, tasks: Int, load: Boolean) {
+      // TODO: Do your work here.
+      println(s"regs was set to $regs")
+      println(s"samples was set to $samples")
+      rates.map{rv =>
+        println(s"Optional option rates was set to $rv")
+      }.getOrElse(println(s"rates was not set."))
+      println(s"target was set to $target")
+      println(s"tasks was set to $tasks")
+      if (load)
+        println(s"load was given on the command line")
     }
 }
